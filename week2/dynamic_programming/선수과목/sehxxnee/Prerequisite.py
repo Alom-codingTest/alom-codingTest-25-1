0a3ac84a6888c0fede806ad0c2c869d69d2ac22a
sehxxnee <shyan5253@naver.com>
2025-03-26 15:12:09 +0900

    선수과목/3h

--- week2/dynamic_programming/선수과목/sehxxnee/Prerequisite.py
+++ week2/dynamic_programming/선수과목/sehxxnee/Prerequisite.py
@@ -1,16 +1,31 @@
 import sys 
+from collections import deque
 
 # n이 과목의 수고 m이 선수 조건의 수
 N,M=map(int,sys.stdin.readline().strip().split())
+table=[[] for _ in range(N+1)]
+res= [0]*(N+1)
+pre = [0]* (N+1)
+
  
-# 0이 n개 들어가있는 리스트 선언
-list=N*[0] #진입차수
-#최소
-# [0,0,0,0,0]
+
 for _ in range (M):
     #A가 B의 선수과목
     A,B=map(int,sys.stdin.readline().strip().split())
-    list[B-1]+=1
+    table[A].append(B)
+    pre[B]+=1
 
-print(list)
+queue=deque()
+for i in range(1, N+1):
+    if pre[i]==0:
+        queue.append([i,1])
 
+while queue:
+    node, cnt=queue.popleft()
+    res[node]=cnt
+    for i in table[node]:
+        pre[i]-=1
+        if pre[i]==0:
+            queue.append([i, cnt+1])
+
+print(*res[1:])
