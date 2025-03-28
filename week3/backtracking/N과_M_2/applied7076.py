fccbc1fd779e9c9dc0a199c6b9c80f1657ca3174
AppliedAlpha <me@applied.kr>
2025-03-28 20:36:57 +0900

    A와 B 2 / 20m

--- week3/backtracking/N과_M_2/applied7076.py
+++ week3/backtracking/N과_M_2/applied7076.py
@@ -1,20 +1,23 @@
-global n, m, arr
+s = input()
+t = input()
+queue = [t]
 
-# 지금 now까지 count개를 선택했다 
-def backtrack(now, cnt):
-    if cnt == m:
-        print(*arr)
-        return
-    
-    for i in range(now + 1, n + 1):
-        arr[cnt] = i
-        backtrack(i, cnt + 1)
+# S -> T: +A, or +B (reverse)
+# T -> S: 맨 끝에서 -A, 맨 앞에서 -B 후 reverse
 
+while queue:
+    now = queue.pop(0)
 
-if __name__ == "__main__": 
-    n, m = map(int, input().split())
-    arr = [0] * m
+    if now == s:
+        print(1)
+        break
 
-    for i in range(1, n + 1):
-        arr[0] = i
-        backtrack(i, 1)
+    if len(now) < len(s):
+        continue
+
+    if now[-1] == 'A':
+        queue.append(now[:-1])
+    if now[0] == 'B':
+        queue.append(now[1:][::-1])
+else:
+    print(0)
