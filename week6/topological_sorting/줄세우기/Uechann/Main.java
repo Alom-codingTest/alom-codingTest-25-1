74a87f6be554b7a173a22e9dc430857452b36ef5
나의찬 <cab07036@naver.com>
2025-05-19 16:34:15 +0900

    줄세우기 / 30M

---
+++ week6/topological_sorting/줄세우기/Uechann/Main.java
@@ -0,0 +1,84 @@
+package topological_sorting.줄세우기.Uechann;
+
+/*
+N명의 학생들을 키 순서대로 줄을 세우려고 한다.
+각 학생의 키를 직접 재서 정렬하면 간단하겠지만, 마땅한 방법이 없어서 두 학생의 키를 비교하는 방법을 사용하기로 하였다.
+그나마도 모든 학생들을 다 비교해 본 것이 아니고, 일부 학생들의 키만을 비교해 보았다.
+일부 학생들의 키를 비교한 결과가 주어졌을 때, 줄을 세우는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 32,000), M(1 ≤ M ≤ 100,000)이 주어진다. M은 키를 비교한 횟수이다.
+다음 M개의 줄에는 키를 비교한 두 학생의 번호 A, B가 주어진다. 이는 학생 A가 학생 B의 앞에 서야 한다는 의미이다.
+학생들의 번호는 1번부터 N번이다.
+
+출력
+첫째 줄에 학생들을 앞에서부터 줄을 세운 결과를 출력한다. 답이 여러 가지인 경우에는 아무거나 출력한다.*/
+
+/*
+* 예제 입력
+* 3 2
+* 1 3
+* 2 3
+*
+* 예제 출력
+* 1 2 3
+* */
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] indegree;
+    static List<List<Integer>> graph;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        indegree = new int[N + 1];
+
+
+        graph = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        //간선 정보 입력
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            graph.get(A).add(B);
+            indegree[B]++;
+        }
+
+        topologicalSort();
+        System.out.println(sb);
+    }
+
+    private static void topologicalSort() {
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            sb.append(current).append(" ");
+
+            //현재 노드에서 나가는 간선들을 직접 순회
+            for (int next : graph.get(current)) {
+                indegree[next]--;
+                if (indegree[next] == 0) {
+                    queue.offer(next);
+                }
+            }
+        }
+    }
+}
